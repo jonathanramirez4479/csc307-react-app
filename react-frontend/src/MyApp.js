d4b51b1da6f2ab6e502cf7d780d837dfcca0d406
Jonathan <jonathanramirez4479@gmail.com>
2023-10-08 19:58:20 -0700

    individual assignment exercises not finished

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -8,16 +8,20 @@ function MyApp() {
     const [characters, setCharacters] =  useState([]);
 
     function removeOneCharacter(index){
-        const updated = characters.filter((characters, i) => {
+      makeDeleteCall().then( result => { // Why is it not deleting in backend?
+        if(result && result.status === 204){
+            const updated = characters.filter((characters, i) => {
             return i !== index
-        });
-        setCharacters(updated);
+            });
+          setCharacters(updated);
+        }
+      });
     }
 
     function updateList(person) {
       makePostCall(person).then( result => {
-      if (result && result.status === 200)
-          setCharacters([...characters, person]);
+      if (result && result.status === 201)
+          setCharacters([...characters, person]); // Not sure what problem 3 asks for??
       });
     }
 
@@ -43,6 +47,16 @@ function MyApp() {
        return false;
     }
  }
+  async function makeDeleteCall(){
+    try {
+      const response = await axios.delete('http://localhost:8000/users/:id');
+      return response;
+    }
+    catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
 
    useEffect(() => {
     fetchAll().then( result => {
